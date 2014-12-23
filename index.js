fd4b4e87731a41eeebb6235ad8b69d4128986bc3
Oskar Sjöberg <oskar.johansson@studentlitteratur.se>
2014-12-23 09:26:25 +0100

    Refactor

--- index.js
+++ index.js
@@ -2,60 +2,96 @@
 var cli = require('cli').enable('version', 'status'), options = cli.parse({
   input: ['i', 'path to .pptx file', 'path']
 });
-var fs = require('fs');
-var unzip = require("unzip");
-var DOMParser = require('xmldom').DOMParser;
 
+cli.main(function (args, options) {
 
-cli.main(function(args, options) {
   cli.debug(JSON.stringify(options));
 
-  var noteData = [];
-  if (!options.input) {
-    return;
-  }
+  function readPptxFile(filename, entryMatchFunc, docHandler, callback) {
 
-  var unzipParser = unzip.Parse();
-  fs.createReadStream(options.input)
-    .pipe(unzipParser)
-    .on('entry', function (entry) {
-      if (entry.path.match(/^ppt\/notesSlides\/notesSlide\d+\.xml/)) {
-        (function(entry){
-          cli.debug('Found '+entry.path);
-          var xml = '';
-          entry.setEncoding('utf8');
-          entry.on('data', function(chunk) {
-            xml += chunk;
-          });
-          entry.on('end', function() {
-            var doc = new DOMParser().parseFromString(xml);
-            var elms = doc.getElementsByTagName('a:t');
-            var note = '';
-            var slideNumber;
-            for (var i in elms) {
-              if (elms.hasOwnProperty(i) && elms[i].textContent != undefined) {
-                if (elms[i].parentNode.getAttribute('type') == 'slidenum') {
-                  slideNumber = parseInt(elms[i].textContent, 10);
-                } else {
-                  note += elms[i].textContent;
-                }
-              }
-            }
-            noteData.push({
-              'slideNumber' : slideNumber,
-              'note' : note
+    var fs = require('fs');
+    var unzip = require("unzip");
+    var DOMParser = require('xmldom').DOMParser;
+
+    var unzipParser = unzip.Parse();
+    var result = [];
+
+    fs.createReadStream(filename)
+      .pipe(unzipParser)
+      .on('entry', function (entry) {
+        if (entryMatchFunc(entry)) {
+          (function (entry) {
+            cli.debug('Found '+entry.path);
+            var xml = '';
+            entry.setEncoding('utf8');
+            entry.on('data', function (chunk) {
+              xml += chunk;
             });
-          });
-        })(entry);
-      } else {
-        entry.autodrain();
+            entry.on('end', function () {
+              var data = docHandler( new DOMParser().parseFromString( xml ) );
+              if (data) {
+                result.push(data);
+              }
+            });
+          }) (entry);
+        } else {
+          entry.autodrain();
+        }
+      });
+
+    unzipParser.on('close', function () {
+      if (callback) {
+        callback(result);
       }
     });
-  unzipParser.on('close', function () {
-    noteData.sort(function(a,b){
-      return a.slideNumber - b.slideNumber;
+  }
+
+
+  function extractNotes(filename, callback) {
+    cli.debug('Extracting notes from: '+ filename);
+
+    readPptxFile(filename, entryMatchFunc, docHandler, resultHandler);
+
+    function entryMatchFunc(entry) {
+      return entry.path.match(/^ppt\/notesSlides\/notesSlide\d+\.xml/);
+    }
+
+    function docHandler(doc) {
+      var elements = doc.getElementsByTagName('a:t');
+      var note = '';
+      var slideNumber;
+      for (var i in elements) {
+        if (elements.hasOwnProperty(i) && elements[i].textContent) {
+          if (elements[i].parentNode.getAttribute('type') == 'slidenum') {
+            slideNumber = parseInt(elements[i].textContent, 10);
+          } else {
+            note += elements[i].textContent;
+          }
+        }
+      }
+      if (note.trim() === '') {
+        return null;
+      }
+      return {
+        'slideNumber' : slideNumber,
+        'note' : note
+      };
+    }
+
+    function resultHandler(result) {
+      result.sort(function (a,b) {
+        return a.slideNumber - b.slideNumber;
+      });
+      if (callback) {
+        callback(result);
+      }
+    }
+  }
+
+  if (options.input) {
+    extractNotes(options.input, function (result) {
+      console.log(result);
     });
-    console.log(noteData);
-  });
+  }
 
 });
